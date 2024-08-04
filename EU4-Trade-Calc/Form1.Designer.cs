e7b8c76f1e625182f1bb92bb4e6483f2b493366c
niko391a <nals@itu.dk>
2024-08-04 14:21:55 +0200

    Initial commit

---
+++ EU4-Trade-Calc/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace EU4_Trade_Calc
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.upstreamTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.downstreamTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // upstreamTextBox
+            // 
+            this.upstreamTextBox.Location = new System.Drawing.Point(73, 65);
+            this.upstreamTextBox.Name = "upstreamTextBox";
+            this.upstreamTextBox.Size = new System.Drawing.Size(164, 20);
+            this.upstreamTextBox.TabIndex = 0;
+            this.upstreamTextBox.TextChanged += new System.EventHandler(this.upstreamTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DOWNSTREAM NODE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "TARGET NODE";
+            // 
+            // downstreamTextBox
+            // 
+            this.downstreamTextBox.Location = new System.Drawing.Point(73, 132);
+            this.downstreamTextBox.Name = "downstreamTextBox";
+            this.downstreamTextBox.Size = new System.Drawing.Size(164, 20);
+            this.downstreamTextBox.TabIndex = 4;
+            this.downstreamTextBox.TextChanged += new System.EventHandler(this.downstreamTextBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Credit to u/Pagoose";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Should you transfer or collect?";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(0, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 26);
+            this.labelResult.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelResult);
+            this.panel1.Location = new System.Drawing.Point(12, 224);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 161);
+            this.panel1.TabIndex = 8;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.upstreamTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.downstreamTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "form1";
+            this.Text = "EU4 Trade Calc";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox upstreamTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox downstreamTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ EU4-Trade-Calc/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EU4_Trade_Calc
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void downstreamTextBox_TextChanged(object sender, EventArgs e)
+        {
+            CalculateTrade();
+        }
+        private void upstreamTextBox_TextChanged(object sender, EventArgs e)
+        {
+            CalculateTrade();
+        }
+        public void CalculateTrade() {
+            // Calculate trade
+            try
+            {
+                if (upstreamTextBox.Text.Length > 0 && downstreamTextBox.Text.Length > 0)
+                {
+                    double currentTradeShare = Convert.ToDouble(upstreamTextBox.Text) / 100;
+                    double downstreamTradeShare = Convert.ToDouble(downstreamTextBox.Text) / 100;
+                    double tradePower = 1 / (2 - currentTradeShare);
+                    if (currentTradeShare >= 0 && currentTradeShare <= 1 && downstreamTradeShare >= 0 && downstreamTradeShare <= 1)
+                    {
+                        if (downstreamTradeShare < tradePower) labelResult.Text = "Collect in the target node!";
+                        else labelResult.Text = "Steer to next node!";
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                // Do no thing
+            }
+                                  
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+                // URL to open
+                string url = "https://www.reddit.com/r/eu4/comments/v6bg3p/biggest_myth_in_eu4_click_to_save_braincells/";
+
+                // Open the URL in the default web browser
+                try
+                {
+                    Process.Start(url);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to open URL. Error: {ex.Message}");
+                }
+            }
+       
+    }
+}
